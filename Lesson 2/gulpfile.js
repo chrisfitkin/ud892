2e595fc14b7ffae5b0327fe79e67b7de7492b44d
chrisfitkin <cfitkin@gmail.com>
2016-09-20 21:43:39 -0700

    gulp file additions

--- Lesson 2/gulpfile.js
+++ Lesson 2/gulpfile.js
@@ -1,16 +1,52 @@
+/*eslint-env node */
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
-	console.log('hello world!');
+  console.log('hello world!');
 });
+var foo = 1234;
+console.log(foo);
 
 gulp.task('styles', function() {
-	gulp.src('sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(gulp.dest('./css'));
+  gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+brute = 123;
+
+gulp.task('start', function() {
+  browserSync.init({
+    server: "./"
+  });
+  browserSync.stream();
+  gulp.watch('sass/**/*.scss', ['styles']);
+  // gulp.watch('js/**/*.js', ['lint']);
+  gulp.watch("app/*.html").on('change', browserSync.reload);
+  // gulp.task('styles', function() {});
+});
+
+gulp.task('lint', () => {
+  // ESLint ignores files with "node_modules" paths.
+  // So, it's best to have gulp ignore the directory as well.
+  // Also, Be sure to return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  return gulp.src(['**/*.js', '!node_modules/**'])
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError());
 });
